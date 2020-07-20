38d16dc594c0abda5e989760c0f1f8bbd4b3378e
David Felix <david.felix02@hotmail.com>
2020-07-20 18:45:28 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,18 +6,16 @@ def find_min_in_nested_arrays(src)
   count = 0
   
   src.size.times do |index|
-    lowest_element = src[index][count] 
     
     src[index].size-1.times do |iner|
       
-      if lowest_element >= src[index][iner+1]
+      if  >= src[index][iner+1]
         save_min_value = src[index][iner+1]
       end
       
     end
     
     array_of_min << save_min_value
-    count+=1
     
   end
   
